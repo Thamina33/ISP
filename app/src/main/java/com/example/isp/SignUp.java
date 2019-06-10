19354a4ee5b2826923e2cc87529d056227219e04
Thamina Omar toma <37588746+Thamina33@users.noreply.github.com>
2019-06-10 21:46:38 +0600

    V2.0.1

---
+++ app/src/main/java/com/example/isp/SignUp.java
@@ -0,0 +1,97 @@
+package com.example.isp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUp extends AppCompatActivity {
+
+    EditText signup_email,signup_pass,con_pass;
+    Button signup_btn;
+    TextView signIn;
+    FirebaseAuth mauth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        mauth = FirebaseAuth.getInstance();
+
+        signup_email= (EditText)findViewById(R.id.email);
+        signup_pass = (EditText)findViewById(R.id.pass);
+        signup_btn = (Button)findViewById(R.id.signUp);
+        con_pass = (EditText)findViewById(R.id.con_pass);
+        signIn = (TextView)findViewById(R.id.con_pass);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent o = new Intent(getApplicationContext() , signIn.class);
+                startActivity(o);
+                finish();
+            }
+        });
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String email ,pass,confirm_pass;
+
+                email =signup_email.getText().toString();
+                pass = signup_pass.getText().toString();
+                confirm_pass = con_pass.getText().toString();
+
+
+
+
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)){
+
+                    mauth.createUserWithEmailAndPassword(email , pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+
+                                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+
+                            else{
+                                String e = task.getException().getMessage();
+                                Toast.makeText(getApplicationContext(),"Error"+e, Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+
+                }
+                else{
+
+                    Toast.makeText(getApplicationContext(),"Please Enter Some Value" , Toast.LENGTH_SHORT).show();
+
+
+                }
+                }
+
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/isp/signIn.java
@@ -0,0 +1,85 @@
+package com.example.isp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class signIn extends AppCompatActivity {
+    EditText email,pass;
+    TextView sign_up;
+    Button login;
+    FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = (EditText)findViewById(R.id.email);
+        pass = (EditText)findViewById(R.id.pass);
+        sign_up = (TextView) findViewById(R.id.signup);
+        login = (Button)findViewById(R.id.signinbtn);
+
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent o = new Intent(getApplicationContext() , SignUp.class);
+                startActivity(o);
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sign_mail , sign_pass;
+                sign_mail = email.getText().toString();
+                sign_pass = pass.getText().toString();
+
+
+                if(!TextUtils.isEmpty(sign_mail)&& !TextUtils.isEmpty(sign_pass)){
+
+                    mAuth.signInWithEmailAndPassword(sign_mail,sign_pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+
+                                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(i);
+                                finish();
+
+                            }
+
+                            else{
+                                String error = task.getException().getMessage() ;
+                                Toast.makeText(getApplicationContext(),"Error"+error , Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+                }
+
+                else{
+
+                    Toast.makeText(getApplicationContext() , "please Enter some value" , Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
